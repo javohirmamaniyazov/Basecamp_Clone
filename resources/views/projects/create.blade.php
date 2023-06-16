ae913dd8742b9a5e65f6067ec9c721ea1fe2318f
Javohir Mamaniyazov <118096306+javohirmamaniyazov@users.noreply.github.com>
2023-06-16 09:09:37 +0500

    Update create.blade.php

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -10,7 +10,7 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8"> 
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 ">
                     <form action="{{ route('projects.store') }}" method="POST">
